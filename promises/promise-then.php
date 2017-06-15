8401eabea8cf3c47d2cc07945488e31b6199b468
Zhuk Sergey <seregazhuk88@gmail.com>
2017-06-15 21:41:35 +0300

    resolution forwarding

---
+++ promises/promise-then.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$deferred = new \React\Promise\Deferred();
+
+$deferred->promise()
+    ->then(function($data){
+        echo $data . PHP_EOL;
+        return $data . ' world';
+    })
+    ->then(function($data){
+        echo $data . PHP_EOL;
+        return strtoupper($data);
+    })->then(function($data){
+        echo $data . PHP_EOL;
+    });
+
+$deferred->resolve('hello');
