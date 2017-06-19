67b362c332189b466010cd8d52be817f87e67789
Zhuk Sergey <seregazhuk88@gmail.com>
2017-06-19 22:49:56 +0300

    add: socket examples, connections pool

--- socket/client.php
+++ socket/client.php
@@ -7,7 +7,8 @@ use React\Socket\ConnectionInterface;
 $loop = React\EventLoop\Factory::create();
 $connector = new React\Socket\Connector($loop);
 
-$connector->connect('127.0.0.1:8080')
+$connector
+    ->connect('127.0.0.1:8080')
     ->then(function (ConnectionInterface $conn) use ($loop) {
         $conn->pipe(new React\Stream\WritableResourceStream(STDOUT, $loop));
         $conn->write("Hello World!\n");
--- socket/server.php
+++ socket/server.php
@@ -4,13 +4,50 @@ require  '../vendor/autoload.php';
 
 use React\Socket\ConnectionInterface;
 
+class ConnectionsPool {
+    /** @var SplObjectStorage  */
+    protected $connections;
+
+    public function __construct()
+    {
+        $this->connections = new SplObjectStorage();
+    }
+
+    public function add(ConnectionInterface $connection)
+    {
+        $connection->write("Hi\n");
+        $this->initEvents($connection);
+        $this->connections->attach($connection);
+    }
+
+    /**
+     * @param ConnectionInterface $connection
+     */
+    protected function initEvents(ConnectionInterface $connection)
+    {
+        // On receiving the data we loop through other connections
+        // end write this data to them
+        $connection->on('data', function ($data) use ($connection) {
+            foreach ($this->connections as $conn) {
+                if ($conn == $connection) continue;
+
+                $conn->write($data);
+            }
+        });
+
+        // When connection closes detach it from the loop
+        $connection->on('close', function($connection){
+            $this->connections->detach($connection);
+        });
+    }
+}
+
+
 $loop = React\EventLoop\Factory::create();
 $socket = new React\Socket\Server('127.0.0.1:8080', $loop);
+$pool = new ConnectionsPool();
 
-$socket->on('connection', function (ConnectionInterface $conn) {
-    $conn->on('data', function ($data) use ($conn) {
-        $conn->write(strtoupper($data));
-    });
+$socket->on('connection', function(ConnectionInterface $connection) use ($pool){
+    $pool->add($connection);
 });
-
 $loop->run();
