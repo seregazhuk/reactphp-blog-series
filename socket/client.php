d65231a4164ceffcd84d6050ade44b8b0b6101f4
Zhuk Sergey <seregazhuk88@gmail.com>
2017-06-24 18:10:58 +0300

    add: telnet-like client

--- socket/client.php
+++ socket/client.php
@@ -7,11 +7,19 @@ use React\Socket\ConnectionInterface;
 $loop = React\EventLoop\Factory::create();
 $connector = new React\Socket\Connector($loop);
 
+$stdin = new \React\Stream\ReadableResourceStream(STDIN, $loop);
+$stdout = new \React\Stream\WritableResourceStream(STDOUT, $loop);
+$composite = new \React\Stream\CompositeStream($stdin, $stdout);
+
 $connector
     ->connect('127.0.0.1:8080')
-    ->then(function (ConnectionInterface $conn) use ($loop) {
-        $conn->pipe(new React\Stream\WritableResourceStream(STDOUT, $loop));
-        $conn->write("Hello World!\n");
-});
+    ->then(
+        function (ConnectionInterface $conn) use ($composite) {
+            $conn->pipe($composite)->pipe($conn);
+        },
+        function (Exception $exception) use ($loop){
+            echo "Cannot connect to server: " . $exception->getMessage();
+            $loop->stop();
+        });
 
 $loop->run();
