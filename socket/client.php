72b527a0d0779ad5d79a2a052f58d66132a8353f
Zhuk Sergey <seregazhuk88@gmail.com>
2017-06-24 22:16:56 +0300

    upd: telnet-like client (readable and writable streams instead of composite)

--- socket/client.php
+++ socket/client.php
@@ -9,13 +9,12 @@ $connector = new React\Socket\Connector($loop);
 
 $stdin = new \React\Stream\ReadableResourceStream(STDIN, $loop);
 $stdout = new \React\Stream\WritableResourceStream(STDOUT, $loop);
-$composite = new \React\Stream\CompositeStream($stdin, $stdout);
 
 $connector
     ->connect('127.0.0.1:8080')
     ->then(
-        function (ConnectionInterface $conn) use ($composite) {
-            $conn->pipe($composite)->pipe($conn);
+        function (ConnectionInterface $conn) use ($stdout, $stdin) {
+            $stdin->pipe($conn)->pipe($stdout);
         },
         function (Exception $exception) use ($loop){
             echo "Cannot connect to server: " . $exception->getMessage();
