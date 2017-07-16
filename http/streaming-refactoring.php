09c772910c50740728eb5dfaaa149d30132d9d3d
Zhuk Sergey <seregazhuk88@gmail.com>
2017-07-16 11:29:40 +0300

    add: streaming server refactoring

--- http/streaming-refactoring.php
+++ http/streaming-refactoring.php
@@ -2,73 +2,87 @@
 
 require '../vendor/autoload.php';
 
-use React\EventLoop\LoopInterface;
 use React\Http\Server;
 use React\Http\Response;
 use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+use React\Stream\ReadableResourceStream;
 use Psr\Http\Message\ServerRequestInterface;
 
 class VideoStreaming {
+
     /**
      * @var LoopInterface
      */
-    private $eventLoop;
+    protected $eventLoop;
 
+    /**
+     * @param LoopInterface $eventLoop
+     */
     public function __construct(LoopInterface $eventLoop)
     {
         $this->eventLoop = $eventLoop;
     }
 
+    /**
+     * @param ServerRequestInterface $request
+     * @return Response
+     */
+    function __invoke(ServerRequestInterface $request)
+    {
+        $file = $this->getFileName($request);
+        if(empty($file)) {
+            return new Response(200, ['Content-Type' => 'text/plain'], 'Video streaming');
+        }
+
+        try {
+            return $this->makeStreamResponse($file);
+        } catch (RuntimeException $exception) {
+            return new Response(404, ['Content-Type' => 'text/plain'], $exception->getMessage());
+        }
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return string
+     */
     protected function getFileName(ServerRequestInterface $request)
     {
         $params = $request->getQueryParams();
-        if(empty($params)) {
-            return new Response(200, ['Content-Type' => 'text/plain'], 'Video streaming');
-        }
-
-        return $params['file'];
+        return $params['file'] ?? '';
     }
 
-    function __invoke(ServerRequestInterface $request)
+    /**
+     * @param string $fileName
+     * @return Response
+     */
+    protected function makeStreamResponse($fileName)
     {
-        $params = $request->getQueryParams();
-        if(empty($params)) {
-            return new Response(200, ['Content-Type' => 'text/plain'], 'Video streaming');
-        }
-
-        $file = $params['video'];
-        $filePath = __DIR__ . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . $file;
+        $filePath = $this->getVideoPath($fileName);
 
         if(!file_exists($filePath)) {
-            return new Response(404, ['Content-Type' => 'text/plain'], "Video $file doesn't exist on server.");
+            throw new RuntimeException("Video $fileName doesn't exist on server.");
         }
 
-        $video = new \React\Stream\ReadableResourceStream(fopen($filePath, 'r'), $this->eventLoop);
+        $stream = new ReadableResourceStream(fopen($filePath, 'r'), $this->eventLoop);
+        return new Response(200, ['Content-Type' => mime_content_type($filePath)], $stream);
+    }
 
-        return new Response(200, ['Content-Type' => mime_content_type($filePath)], $video);
+    /**
+     * @param string $fileName
+     * @return string
+     */
+    protected function getVideoPath($fileName)
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . $fileName;
     }
 
 }
 
 $loop = Factory::create();
 
-$server = new Server(function (ServerRequestInterface $request) use ($loop) {
-    $params = $request->getQueryParams();
-    if(empty($params)) {
-        return new Response(200, ['Content-Type' => 'text/plain'], 'Video streaming');
-    }
-
-    $file = $params['video'];
-    $filePath = __DIR__ . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . $file;
-
-    if(!file_exists($filePath)) {
-        return new Response(404, ['Content-Type' => 'text/plain'], "Video $file doesn't exist on server.");
-    }
-
-    $video = new \React\Stream\ReadableResourceStream(fopen($filePath, 'r'), $loop);
-
-    return new Response(200, ['Content-Type' => mime_content_type($filePath)], $video);
-});
+$videoStreaming = new VideoStreaming($loop);
+$server = new Server($videoStreaming);
 
 $socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
 $server->listen($socket);
