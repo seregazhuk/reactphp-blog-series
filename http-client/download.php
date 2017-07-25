3b42512f2ec5b612788bec7fe81bb9044206f67d
Zhuk Sergey <seregazhuk88@gmail.com>
2017-07-25 15:16:53 +0300

    add: http-client download with progress example

--- http-client/download.php
+++ http-client/download.php
@@ -9,8 +9,18 @@ $file = new \React\Stream\WritableResourceStream(fopen('sample.mp4', 'w'), $loop
 $request = $client->request('GET', 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4');
 
 $request->on('response', function (\React\HttpClient\Response $response) use ($file) {
-    $response->pipe($file);
+    $currentSize = 0;
+    $size = $response->getHeaders()['Content-Length'];
+
+    $through = new \React\Stream\ThroughStream();
+    $through->on('data', function($data) use ($size, &$currentSize){
+        $currentSize += strlen($data);
+        echo "\033[1A", "Downloading: ", number_format($currentSize / $size * 100), "%\n";
+    });
+
+    $response->pipe($through)->pipe($file);
 });
 
 $request->end();
+echo "\n";
 $loop->run();
