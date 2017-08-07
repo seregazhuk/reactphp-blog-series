2d536e9bd4b60ddf2127ec029812107e90f01ff7
Zhuk Sergey <seregazhuk88@gmail.com>
2017-08-07 22:10:05 +0300

    adds child process version simple example

--- child-process/version.php
+++ child-process/version.php
@@ -1,3 +1,22 @@
 <?php
 
 require '../vendor/autoload.php';
+
+use React\EventLoop\Factory;
+use React\ChildProcess\Process;
+
+$loop = Factory::create();
+$process = new Process('php -v');
+
+$process->start($loop);
+$process->stdout->on('data', function($data){
+    echo $data, "\n";
+});
+
+$process->on('exit', function($exitCode, $termSignal) {
+    echo 'Process exited with code ' . $exitCode . PHP_EOL;
+});
+
+
+$loop->run();
+
