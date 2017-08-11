64a8815cb9a7f596e374b491dcf84a989be3dba3
Zhuk Sergey <seregazhuk88@gmail.com>
2017-08-11 21:34:08 +0300

    add child process pipe example

---
+++ child-process/pipe.php
@@ -0,0 +1,21 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use React\EventLoop\Factory;
+use React\ChildProcess\Process;
+
+$loop = Factory::create();
+$ls = new Process('ls'); // list files in current directory
+$wc = new Process('wc -l'); // counts number of lines
+
+$ls->start($loop);
+$wc->start($loop);
+
+$ls->stdout->pipe($wc->stdin);
+
+$wc->stdout->on('data', function($data){
+    echo "Total number of files and folders: " . $data;
+});
+
+$loop->run();
