32e44972e358fa73a56a9c220bdc537cf3f15ea4
Zhuk Sergey <seregazhuk88@gmail.com>
2017-08-11 20:17:53 +0300

    adds child process input example

---
+++ child-process/input-hello-world.php
@@ -0,0 +1,18 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use React\EventLoop\Factory;
+use React\ChildProcess\Process;
+
+$loop = Factory::create();
+$process = new Process('php -a');
+
+$process->start($loop);
+$process->stdout->on('data', function($data) use ($loop) {
+    echo $data;
+});
+
+$process->stdin->write("echo 'Hello World';\n");
+$loop->run();
+
