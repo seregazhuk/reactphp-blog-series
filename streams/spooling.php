d51716b6745c9b67a7a553f8f1964a7997f13e42
Zhuk Sergey <seregazhuk88@gmail.com>
2017-08-30 10:03:52 +0300

    adds spooling example to streams

---
+++ streams/spooling.php
@@ -0,0 +1,22 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use React\Stream\ReadableResourceStream;
+
+$loop = React\EventLoop\Factory::create();
+$spool = "";
+
+$stream = new ReadableResourceStream(
+    fopen('file.txt', 'r'), $loop
+);
+
+$stream->on('data', function($data) use (&$spool) {
+    $spool .= $data;
+});
+
+$stream->on('end', function() use (&$spool) {
+    echo $spool;
+});
+
+$loop->run();
