cb13b5f482530b937e7e866b026f7a0b764a5f69
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-02 18:06:12 +0300

    adds bunny-streaming example

---
+++ http/bunny-streaming.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use React\Http\Server;
+use React\Http\Response;
+use React\EventLoop\Factory;
+use Psr\Http\Message\ServerRequestInterface;
+
+$loop = Factory::create();
+
+$server = new Server(function (ServerRequestInterface $request) use ($loop) {
+    $video = new \React\Stream\ReadableResourceStream(fopen('media/bunny.mp4', 'r'), $loop);
+
+    return new Response(200, ['Content-Type' => 'video/mp4'], $video);
+});
+
+$socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
+$server->listen($socket);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . "\n";
+
+$loop->run();
