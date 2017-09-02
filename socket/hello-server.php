70bd0afacce819fbd234bbe2922d9988980aeac4
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-02 14:59:21 +0300

    adds hello-server to sockets examples

---
+++ socket/hello-server.php
@@ -0,0 +1,16 @@
+<?php
+
+require  '../vendor/autoload.php';
+
+use React\Socket\ConnectionInterface;
+
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server('127.0.0.1:8080', $loop);
+
+$socket->on('connection', function(ConnectionInterface $connection){
+    $connection->write('Hi');
+});
+
+echo "Listening on {$socket->getAddress()}\n";
+
+$loop->run();
