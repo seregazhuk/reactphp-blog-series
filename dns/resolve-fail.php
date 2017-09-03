6dc93c99f374ed8a154fd1c95c5127035eb6a13f
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-03 10:18:44 +0300

    dns examples

--- dns/resolve-fail.php
+++ dns/resolve-fail.php
@@ -6,10 +6,8 @@ $loop = React\EventLoop\Factory::create();
 $factory = new React\Dns\Resolver\Factory();
 $dns = $factory->create('8.8.8.8', $loop);
 
-$resolve = $dns->resolve('php.net')
-    ->then(function ($ip) {
-        echo "php.net: $ip\n";
-    })
+$dns = $factory->create('8.8.8.8', $loop);
+$dns->resolve('some-wrong-domain')
     ->otherwise(function (\React\Dns\RecordNotFoundException $e) {
         echo "Cannot resolve: " . $e->getMessage();
     });
--- dns/resolve-success.php
+++ dns/resolve-success.php
@@ -6,12 +6,10 @@ $loop = React\EventLoop\Factory::create();
 $factory = new React\Dns\Resolver\Factory();
 $dns = $factory->create('8.8.8.8', $loop);
 
-$resolve = $dns->resolve('php.net')
+$dns = $factory->create('8.8.8.8', $loop);
+$dns->resolve('php.net')
     ->then(function ($ip) {
         echo "php.net: $ip\n";
-    })
-    ->otherwise(function (\React\Dns\RecordNotFoundException $e) {
-        echo "Cannot resolve: " . $e->getMessage();
     });
 
 $loop->run();
