b2c98df0b6d271859b8b02f3f157a1b51fd88f68
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-11 21:56:58 +0300

    blocking promise example

--- block/promise.php
+++ block/promise.php
@@ -7,9 +7,9 @@ use function Clue\React\Block\await;
 $loop = React\EventLoop\Factory::create();
 
 $resolve = function(callable $resolve, callable $reject) use ($loop) {
-    ////$loop->addTimer(5, function() use ($resolve) {
+    $loop->addTimer(5, function() use ($resolve) {
         return $resolve('Hello wolrd!');
-    //});
+    });
 };
 
 $cancel = function(callable $resolve, callable $reject) {
