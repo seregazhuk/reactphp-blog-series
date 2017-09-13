fe7d9fd8ca851f625884190c7fb7a9b897739e5d
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-13 16:56:38 +0300

    adds socket chat server with unique names check

---
+++ socket/server-unique-names.php
@@ -0,0 +1,115 @@
+<?php
+
+require  '../vendor/autoload.php';
+
+use React\Socket\ConnectionInterface;
+
+class ConnectionsPool {
+
+    /** @var SplObjectStorage  */
+    protected $connections;
+
+    public function __construct()
+    {
+        $this->connections = new SplObjectStorage();
+    }
+
+    public function add(ConnectionInterface $connection)
+    {
+        $connection->write("Enter your name: ");
+        $this->initEvents($connection);
+        $this->setConnectionData($connection, []);
+    }
+
+    /**
+     * @param ConnectionInterface $connection
+     */
+    protected function initEvents(ConnectionInterface $connection)
+    {
+        // On receiving the data we loop through other connections
+        // from the pool and write this data to them
+        $connection->on('data', function ($data) use ($connection) {
+            $connectionData = $this->getConnectionData($connection);
+
+            // It is the first data received, so we consider it as
+            // a users name.
+            if(empty($connectionData)) {
+                $this->sendJoinMessage($data, $connection);
+                return;
+            }
+
+            $name = $connectionData['name'];
+            $this->sendAll("$name: $data", $connection);
+        });
+
+        // When connection closes detach it from the pool
+        $connection->on('close', function() use ($connection){
+            $data = $this->getConnectionData($connection);
+            $name = $data['name'] ?? '';
+
+            $this->connections->offsetUnset($connection);
+            $this->sendAll("User $name leaves the chat\n", $connection);
+        });
+    }
+
+    protected function checkUniqueName($name)
+    {
+        foreach ($this->connections as $obj) {
+            $data = $this->connections->offsetGet($obj);
+            $takenName = $data['name'] ?? '';
+            if($takenName == $name) return false;
+        }
+
+        return true;
+    }
+
+    protected function sendJoinMessage($name, $connection)
+    {
+        $name = str_replace(["\n", "\r"], "", $name);
+
+        if(!$this->checkUniqueName($name)) {
+            $connection->write("Name $name is already taken!\n");
+            $connection->write("Enter your name: ");
+            return;
+        }
+
+        $this->setConnectionData($connection, ['name' => $name]);
+        $this->sendAll("User $name joins the chat\n", $connection);
+    }
+
+    protected function setConnectionData(ConnectionInterface $connection, $data)
+    {
+        $this->connections->offsetSet($connection, $data);
+    }
+
+    protected function getConnectionData(ConnectionInterface $connection)
+    {
+        return $this->connections->offsetGet($connection);
+    }
+
+    /**
+     * Send data to all connections from the pool except
+     * the specified one.
+     *
+     * @param mixed $data
+     * @param ConnectionInterface $except
+     */
+    protected function sendAll($data, ConnectionInterface $except) {
+        foreach ($this->connections as $conn) {
+            if ($conn != $except) $conn->write($data);
+        }
+    }
+}
+
+
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server('127.0.0.1:8080', $loop);
+$pool = new ConnectionsPool();
+
+$socket->on('connection', function(ConnectionInterface $connection) use ($pool){
+    $pool->add($connection);
+});
+
+echo "Listening on {$socket->getAddress()}\n";
+
+$loop->run();
