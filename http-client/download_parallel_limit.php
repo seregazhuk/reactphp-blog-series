ce746ca9bba3cd7f2b6a90dae2e4435d944d54ba
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-25 22:15:36 +0300

    parallel files download with limit

---
+++ http-client/download_parallel_limit.php
@@ -0,0 +1,113 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use React\EventLoop\LoopInterface;
+use React\HttpClient\Client;
+use React\HttpClient\Request;
+use React\HttpClient\Response;
+use React\Stream\WritableResourceStream;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class Downloader
+{
+    /**
+     * @var React\EventLoop\LoopInterface;
+     */
+    private $loop;
+
+    /**
+     * @var \React\HttpClient\Client
+     */
+    protected $client;
+
+    /**
+     * @var ConsoleOutput
+     */
+    protected $output;
+
+    /**
+     * @var ProgressBar
+     */
+    protected $bar;
+
+    /**
+     * @var array
+     */
+    protected $files = [];
+
+    /**
+     * @param Client $client
+     * @param LoopInterface $loop
+     * @param ConsoleOutput $output
+     */
+    public function __construct(Client $client, LoopInterface $loop, ConsoleOutput $output)
+    {
+        $this->client = $client;
+        $this->loop = $loop;
+        $this->output = $output;
+    }
+
+    /**
+     * @param array $files
+     * @param int $limit
+     */
+    public function download(array $files, $limit = 0)
+    {
+        $this->files = $files;
+        $this->bar = new ProgressBar($this->output, count($this->files));
+        $this->bar->start();
+
+        $max = $limit ? $limit : count($this->files);
+        while($max --) {
+            $this->runDownload();
+        }
+
+        $this->loop->run();
+    }
+
+    /**
+     * @param string $url
+     * @return Request
+     */
+    public function initRequest($url)
+    {
+        $request = $this->client->request('GET', $url);
+        $fileName = basename($url);
+
+        $file = new WritableResourceStream(fopen($fileName, 'w'), $this->loop);
+        $request->on('response', function (Response $response) use ($file, $fileName) {
+            $response->pipe($file);
+
+            $response->on('end', function () {
+                $this->bar->advance();
+                if ($this->files) $this->runDownload();
+            });
+        });
+
+        return $request;
+    }
+
+    protected function runDownload()
+    {
+        $file = array_pop($this->files);
+        $request = $this->initRequest($file);
+        $request->end();
+    }
+}
+
+$loop = React\EventLoop\Factory::create();
+$client = new React\HttpClient\Client($loop);
+
+$files = [
+    'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+];
+
+(new Downloader($client, $loop, new ConsoleOutput()))->download($files, 2);
+
