dbcd0405453c09d399af432066f723436ed436d4
Zhuk Sergey <seregazhuk88@gmail.com>
2017-12-07 12:03:45 +0300

    add promise-stream examples

---
+++ promise-stream/promise-to-stream/unwrap_readable_close.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../../vendor/autoload.php';
+
+$deferred = new \React\Promise\Deferred();
+$stream = \React\Promise\Stream\unwrapReadable($deferred->promise());
+
+$stream->on('close', function() {
+    echo 'Closed';
+});
+
+$loop = \React\EventLoop\Factory::create();
+$stdin = new \React\Stream\ReadableResourceStream(fopen('php://stdin', 'r'), $loop);
+$deferred->resolve($stdin);
+$stdin->close();
+
+$loop->run();
