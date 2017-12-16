2f6f8206917744159334abee5462b9731ffa4f1d
Zhuk Sergey <seregazhuk88@gmail.com>
2017-12-16 16:40:56 +0300

    http middleware examples

---
+++ http-server-middleware/limit_concurrent_requests.php
@@ -0,0 +1,31 @@
+<?php
+require '../vendor/autoload.php';
+
+use React\Http\Server;
+use React\Http\Response;
+use React\EventLoop\Factory;
+use Psr\Http\Message\ServerRequestInterface;
+
+$loop = Factory::create();
+
+$server = new Server([
+    new \React\Http\Middleware\LimitConcurrentRequestsMiddleware(3),
+    function(ServerRequestInterface $request, callable $next) use ($loop) {
+        $deferred = new \React\Promise\Deferred();
+        $loop->addTimer(5, function() use ($next, $request, $deferred) {
+            $deferred->resolve($next($request));
+        });
+
+        return $deferred->promise();
+    },
+    function (ServerRequestInterface $request) {
+        return new Response(200, ['Content-Type' => 'text/plain'],  "Hello world\n");
+    }
+]);
+
+$socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
+$server->listen($socket);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . "\n";
+
+$loop->run();
