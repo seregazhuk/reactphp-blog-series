a0e3dfa2c3cd7b152403fb63c1b56f46ef99cccf
Zhuk Sergey <seregazhuk88@gmail.com>
2017-12-19 23:22:11 +0300

    http middleware examples

---
+++ http-server-middleware/parse_request_body_contents.php
@@ -0,0 +1,25 @@
+<?php
+require '../vendor/autoload.php';
+
+use React\Http\Response;
+use React\EventLoop\Factory;
+use Psr\Http\Message\ServerRequestInterface;
+
+$loop = Factory::create();
+
+$server = new \React\Http\Server([
+    function (ServerRequestInterface $request) {
+        /** @var \Psr\Http\Message\UploadedFileInterface[] $files */
+        $files = $request->getUploadedFiles();
+        $video = $files['video'] ?? null;
+
+        return new Response(200, ['Content-Type' => 'text/plain'], $video ? $video->getStream() : '');
+    }
+]);
+
+$socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
+$server->listen($socket);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
+
+$loop->run();
