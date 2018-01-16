389d643e6a1533880737722254fdd7ef88fd0cef
Zhuk Sergey <seregazhuk88@gmail.com>
2018-01-16 22:05:32 +0300

    managing promises resolve

---
+++ managing-promises/resolve.php
@@ -0,0 +1,14 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$deferred = new \React\Promise\Deferred();
+$promise = React\Promise\resolve($deferred->promise());
+var_dump($deferred->promise() === $promise);
+
+$deferred->resolve('hello world');
+
+$promise = React\Promise\resolve($value = 'my-value');
+$promise->then(function ($value) {
+    echo $value . PHP_EOL;
+});
