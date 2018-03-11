49b3c2208de501bf9c6554eb8798be42be3dab64
Zhuk Sergey <seregazhuk88@gmail.com>
2018-03-11 20:06:23 +0300

    router with http: fast router example wip

--- http-with-router/fast-router.php
+++ http-with-router/fast-router.php
@@ -8,7 +8,7 @@ use React\EventLoop\Factory;
 use Psr\Http\Message\ServerRequestInterface;
 
 $tasks = [];
-$listTasks = function () use ($tasks) {
+$listTasks = function () use (&$tasks) {
     return new Response(200, ['Content-Type' => 'text/plain'],  implode(PHP_EOL, $tasks));
 };
 
@@ -22,14 +22,17 @@ $addTask = function (ServerRequestInterface $request) use (&$tasks) {
     return new Response(400, ['Content-Type' => 'text/plain'], 'Task field is required');
 };
 
-$viewTask = function(ServerRequestInterface $request) use ($tasks) {
-    $path = $request->getUri()->getPath();
-    die();
+$viewTask = function(ServerRequestInterface $request, $params) use (&$tasks) {
+    if (isset($tasks[$id])) {
+        return new Response(200, ['Content-Type' => 'text/plain'], $tasks[$id]);
+    }
+
+    return new Response(404, ['Content-Type' => 'text/plain'], 'Not found');
 };
 
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $routes) use ($listTasks, $addTask, $viewTask) {
-    $routes->addRoute('GET', '/tasks', $listTasks);
     $routes->addRoute('GET', '/tasks/{id:\d+}', $viewTask);
+    $routes->addRoute('GET', '/tasks', $listTasks);
     $routes->addRoute('POST', '/tasks', $addTask);
 });
 
@@ -42,7 +45,8 @@ $server = new Server(function (ServerRequestInterface $request) use ($dispatcher
         case FastRoute\Dispatcher::NOT_FOUND:
             return new Response(404, ['Content-Type' => 'text/plain'],  'Not found');
         case FastRoute\Dispatcher::FOUND:
-            return $routeInfo[1]($request);
+            $params = $routeInfo[2] ?? [];
+            return $routeInfo[1]($request, $params);
     }
 
     return new Response(200, ['Content-Type' => 'text/plain'], 'Tasks list');
