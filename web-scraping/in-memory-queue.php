0df961cd64541b8ffdf54372e31a85d8eec69fca
Zhuk Sergey <seregazhuk88@gmail.com>
2018-03-16 22:07:48 +0300

    add queue to web scraper

--- web-scraping/in-memory-queue.php
+++ web-scraping/in-memory-queue.php
@@ -3,105 +3,24 @@
 require '../vendor/autoload.php';
 
 use Clue\React\Buzz\Browser;
-use React\EventLoop\LoopInterface;
-use React\Promise\Promise;
-use Symfony\Component\DomCrawler\Crawler;
-
-class Parser
-{
-    /**
-     * @var Browser
-     */
-    private $client;
-
-    /**
-     * @var array
-     */
-    private $parsed = [];
-
-    /**
-     * @var LoopInterface
-     */
-    private $loop;
-
-    public function __construct(Browser $client, LoopInterface $loop)
-    {
-        $this->client = $client;
-        $this->loop = $loop;
-    }
-
-    public function parse(array $urls = [], $timeout = 5, $concurrencyLimit = 1)
-    {
-        $queue = $this->initQueue($concurrencyLimit);
-
-        foreach ($urls as $url) {
-            /** @var Promise $promise */
-            $promise = $queue($url)->then(
-                function (\Psr\Http\Message\ResponseInterface $response) {
-                    $this->parsed[] = $this->extractFromHtml((string)$response->getBody());
-                }
-            );
-
-            $this->loop->addTimer($timeout, function () use ($promise) {
-                $promise->cancel();
-            });
-        }
-    }
-
-    public function extractFromHtml($html)
-    {
-        $crawler = new Crawler($html);
-
-        $title = trim($crawler->filter('h1')->text());
-        $genres = $crawler->filter('[itemprop="genre"] a')->extract(['_text']);
-        $description = trim($crawler->filter('[itemprop="description"]')->text());
-
-        $crawler->filter('#titleDetails .txt-block')->each(
-            function (Crawler $crawler) {
-                foreach ($crawler->children() as $node) {
-                    $node->parentNode->removeChild($node);
-                }
-            }
-        );
-
-        $releaseDate = trim($crawler->filter('#titleDetails .txt-block')->eq(3)->text());
-
-        return [
-            'title'        => $title,
-            'genres'       => $genres,
-            'description'  => $description,
-            'release_date' => $releaseDate,
-        ];
-    }
-
-    public function getMovieData()
-    {
-        return $this->parsed;
-    }
-
-    /**
-     * @param int $concurrencyLimit
-     * @return \Clue\React\Mq\Queue
-     */
-    protected function initQueue($concurrencyLimit)
-    {
-        $queue = new Clue\React\Mq\Queue($concurrencyLimit, null, function ($url) {
-            echo 'call' . PHP_EOL;
-            return $this->client->get($url);
-        });
-        return $queue;
-    }
-}
-
+use Clue\React\Mq\Queue;
 
 $loop = React\EventLoop\Factory::create();
-$client = new Browser($loop);
+$browser = new Browser($loop);
 
-$parser = new Parser($client, $loop);
-$parser->parse([
+$queue = new Queue(2, null, function($url) use ($browser) {
+    return $browser->get($url);
+});
+
+$urls = [
     'http://www.imdb.com/title/tt1270797/',
     'http://www.imdb.com/title/tt2527336/',
-], 20, 1);
+];
+
+foreach ($urls as $url) {
+    $queue($url)->then(function($response) {
+        echo $response;
+    });
+}
 
 $loop->run();
-print_r($parser->getMovieData());
