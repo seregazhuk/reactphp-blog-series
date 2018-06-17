41bd78c1ac9ff5a182c3f9f8eda4b14b49df3e8f
Zhuk Sergey <seregazhuk88@gmail.com>
2018-06-17 20:23:56 +0300

    Add recoil examples

---
+++ coroutines/dns-promise.php
@@ -0,0 +1,30 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$loop = \React\EventLoop\Factory::create();
+$resolver = (new React\Dns\Resolver\Factory)->create('8.8.8.8', $loop);
+
+function resolveDomainName($name, React\Dns\Resolver\Resolver $resolver)
+{
+    return $resolver
+        ->resolve($name)
+        ->then(function ($ip) use ($name) {
+                echo 'Resolved "' . $name . '" to ' . $ip . PHP_EOL;
+            },
+            function (Exception $e) use ($name) {
+                echo 'Failed to resolve "' . $name . '" - ' . $e->getMessage() . PHP_EOL;
+            }
+        );
+}
+
+\React\Promise\all([
+    resolveDomainName('recoil.io', $resolver),
+    resolveDomainName('reactphp.org', $resolver),
+    resolveDomainName('probably-wont-resolve', $resolver),
+])->then(
+    function () {
+        echo 'Done' . PHP_EOL;
+    }
+);
+$loop->run();
---
+++ coroutines/dns-yield.php
@@ -0,0 +1,33 @@
+<?php
+
+use Recoil\Recoil;
+
+require '../vendor/autoload.php';
+
+$loop = \React\EventLoop\Factory::create();
+
+function resolveDomainName($name, React\Dns\Resolver\Resolver $resolver)
+{
+    try {
+        $ip = yield $resolver->resolve($name);
+        echo 'Resolved "' . $name . '" to ' . $ip . PHP_EOL;
+    } catch (Exception $e) {
+        echo 'Failed to resolve "' . $name . '" - ' . $e->getMessage() . PHP_EOL;
+    }
+}
+
+\Recoil\React\ReactKernel::start(
+    function () {
+        $resolver = (new React\Dns\Resolver\Factory)->create(
+            '8.8.8.8', yield Recoil::eventLoop()
+        );
+
+        yield [
+            resolveDomainName('recoil.io', $resolver),
+            resolveDomainName('reactphp.org', $resolver),
+            resolveDomainName('probably-wont-resolve', $resolver),
+        ];
+        echo 'Done' . PHP_EOL;
+    }
+);
+
