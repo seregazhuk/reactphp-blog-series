8ab442bd5f676edbde5f8ed57ad363bcd26ace89
Zhuk Sergey <seregazhuk88@gmail.com>
2018-06-28 20:48:35 +0300

    Code style in cache examples

--- cache/no-value.php
+++ cache/no-value.php
@@ -6,6 +6,6 @@ $cache = new React\Cache\ArrayCache();
 $cache->set('foo', 'bar');
 
 $cache->get('baz')->otherwise(function(){
-    echo "There is no value in cache";
+    echo 'There is no value in cache' . PHP_EOL;
 });
 
